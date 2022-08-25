a53cd8e347fe55a8432636f0d4436b035a306ca9
saravanakumar-20 <skrijul222@gmail.com>
2022-08-25 22:13:59 +0530

    payment update

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ app.post ( "/payment_status" , ( req , res ) => {
     res.send ( req.body.status )
 } )
 app.post ( "/payhash" , authenticateToken , ( req , res ) => {
-   let string = process.env.TEST_KEY + "|"  + payload.email + "|" + req.body.amount + "|legacyentry|" + payload.name + "|" + payload.email + "|||||||||||" + process.env.TEST_SALT
+   let string = process.env.MERCHANT_KEY + "|"  + payload.email + "|" + req.body.amount + "|legacyentry|" + payload.name + "|" + payload.email + "|||||||||||" + process.env.SALT
    sha512.update ( string )
    digest = sha512.digest ().toString ( 'hex' )
    paymentHash.create ( { email: payload.email , paymentHash: digest } , ( error , result ) => {
@@ -132,7 +132,7 @@ app.post ( "/payhash" , authenticateToken , ( req , res ) => {
    } )
    sha512 = require ( "crypto" ).createHash ( "sha512" )
 
-   res.send  ( { payurl: 'https://test.payu.in/_payment' , data: { "key": process.env.TEST_KEY , "txnid": payload.email, "amount": req.body.amount , productinfo: "legacyentry" , firstname: payload.name , email: payload.email , phone: payload.phone_number , surl: "https://legacy-mepco.herokuapp.com/payment_status" , furl: "https://legacy-mepco.herokuapp.com/payment_status" , hash: digest } } )
+   res.send  ( { payurl: 'https://test.payu.in/_payment' , data: { "key": process.env.MERCHANT_KEY , "txnid": payload.email, "amount": req.body.amount , productinfo: "legacyentry" , firstname: payload.name , email: payload.email , phone: payload.phone_number , surl: "https://legacy-mepco.herokuapp.com/payment_status" , furl: "https://legacy-mepco.herokuapp.com/payment_status" , hash: digest } } )
 } )
 
 app.post('/loginuser',(req,res)=>{
