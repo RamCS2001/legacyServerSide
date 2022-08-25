eec40ad0a71fb99c5c6f968283490655523db8c5
saravanakumar-20 <skrijul222@gmail.com>
2022-08-25 16:38:02 +0530

    correction

--- models/PaymentHash.js
+++ models/PaymentHash.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-const payment = new mongoose.Schema( { email: { type: string , required: true } ,  paymentHash: { type: String , required: true } } );
+const payment = new mongoose.Schema( { email: { type: String , required: true } ,  paymentHash: { type: String , required: true } } );
 const paymentHash = mongoose.model('collegeEvents',payment);
 module.exports = paymentHash;
