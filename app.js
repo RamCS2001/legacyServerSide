eb98040faf122afad952417f2ce395717b969a79
saravanakumar-20 <skrijul222@gmail.com>
2022-08-25 16:35:06 +0530

    status checked for payment

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const User = require('./models/User');
 const groupEvents = require('./models/groupEvents');
 const count= require('./models/count');
 const collegeEvents = require('./models/collegeEvents');
+const paymentHash = require ( './models/PaymentHash' )
 
 app.get('/', (req,res)=>{
     res.send("Hello")
@@ -109,15 +110,26 @@ app.post('/createuser',(req,res)=>{
         }
     } )
 });
-app.post ( "/payment_status" , ( req , res ) => {
-    console.log ( "method called" )
+app.post ( "/payment_status" , authenticateToken ,  ( req , res ) => {
+    paymentHash.findOne ( { email: payload.email } , ( error , result ) => {
+        if ( error )
+          throw error
+        if ( result.paymentHash == req.body.hash ) {
+            res.send ( { status: req.body.status } )
+        }
+        else 
+           res.send ( "alert! security breach avoid payment!" )
+    } ) 
     res.send ( req.body.status )
 } )
 app.post ( "/payhash" , authenticateToken , ( req , res ) => {
    let string = process.env.MERCHANT_KEY + "|"  + payload.email + "|" + req.body.amount + "|legacyentry|" + payload.name + "|" + payload.email + "|||||||||||" + process.env.SALT
-   
    sha512.update ( string )
    digest = sha512.digest ().toString ( 'hex' )
+   paymentHash.insertOne ( { email: payload.email , paymentHash: digest } , ( error , result ) => {
+    if ( error )
+      console.log ( error )
+   } )
    sha512 = require ( "crypto" ).createHash ( "sha512" )
    console.log ( { payurl: 'https://secure.payu.in/_payment' , data: { "key": process.env.MERCHANT_KEY , "txnid": payload.email, "amount": req.body.amount , "productinfo": "legacyentry" , "firstname": payload.name , "email": payload.email , "phone": payload.phone_number , "surl": "http://127.0.0.1:5000/payment_status" , "furl": "http://127.0.0.1:5000/payment_status" , "hash": digest } } )
    res.send  ( { payurl: 'https://secure.payu.in/_payment' , data: { "key": process.env.MERCHANT_KEY , "txnid": payload.email, "amount": req.body.amount , "productinfo": "legacyentry" , "firstname": payload.name , "email": payload.email , "phone": payload.phone_number , "surl": "https://legacy-mepco.herokuapp.com/payment_status" , "furl": "https://legacy-mepco.herokuapp.com/payment_status" , "hash": digest } } )
--- models/PaymentHash.js
+++ models/PaymentHash.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-const collegeEventsSchema = new mongoose.Schema( { email: { type: string , required: true } ,  paymentHash: { type: String , required: true } } );
-const collegeEvents= mongoose.model('collegeEvents',collegeEventsSchema);
-module.exports = collegeEvents;
+const payment = new mongoose.Schema( { email: { type: string , required: true } ,  paymentHash: { type: String , required: true } } );
+const paymentHash = mongoose.model('collegeEvents',payment);
+module.exports = paymentHash;
