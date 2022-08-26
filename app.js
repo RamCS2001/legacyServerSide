186d4f9093eda2af2da61c1da3e4dc002ba0d178
saravanakumar-20 <skrijul222@gmail.com>
2022-08-26 16:28:17 +0530

    okay

--- app.js
+++ app.js
@@ -126,7 +126,7 @@ app.post ( "/payment_status" , ( req , res ) => {
       else {
         if ( result [ result.length - 1 ] [ req.body.status ] == req.body.hash ) {
            if ( req.body.status == "success" )
-             User.findOneAndUpdate ( { email: req.body.mail } , { regFeesPayment: true, accommodationFeesPayment: ( parseInt ( req.body.amount ) > 300 ) })
+             User.findOneAndUpdate ( { email: req.body.email } , { regFeesPayment: true, accommodationFeesPayment: ( parseInt ( req.body.amount ) > 300 ) })
            res.redirect ( "https://legacy-mepco.vercel.app/paid?status=" + req.body.status )
         }
         else
