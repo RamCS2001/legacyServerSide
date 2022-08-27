42a1e75fb7c02638c282542b087155b8d39d7189
saravanakumar-20 <skrijul222@gmail.com>
2022-08-27 21:13:17 +0530

    token log removed

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ function getHash ( timestamp , status , amount , payload , reverse ) {
   if ( reverse ) {
      formulatedString = process.env.SALT + "|" + status + "|||||||||||" + payload.email + "|" + payload.name + "|legacyentry|" + amount + ".00|" + ( payload.email + timestamp ) + "|" + process.env.MERCHANT_KEY
    }
-   console.log ( formulatedString )
    sha512.update ( formulatedString )
    return reverse ? sha512.digest ( ).toString ( "hex" ) : { time: timestamp , digest: sha512.digest ( ).toString ( "hex" ) }
 }
